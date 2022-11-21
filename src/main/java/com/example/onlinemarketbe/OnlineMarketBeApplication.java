82d9c6d3903076faaac30912da3369f40e69a8e1
dovuong <70367803+dovuong@users.noreply.github.com>
2022-11-21 15:40:12 +0700

    initial

---
+++ src/main/java/com/example/onlinemarketbe/OnlineMarketBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinemarketbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinemarketbe/OnlineMarketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinemarketbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
